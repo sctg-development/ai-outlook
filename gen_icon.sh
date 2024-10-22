27da446f26078da85106ff90ec681e8c7fda3bd9
æLtorio <aeltorio@universidadatlantico.org>
2024-10-22 11:29:30 +0200

    add favicon.ico

--- gen_icon.sh
+++ gen_icon.sh
@@ -6,8 +6,9 @@
 pushd assets > /dev/null
 for size in 16 32 64 80 128; do
     # from ImageMagick
-    convert logo-filled.png -resize ${size}x${size} icon-$size.png
+    magick logo-filled.png -resize ${size}x${size} icon-$size.png
     DATA_URL="data:image/png;base64,$(base64 -i icon-$size.png)"
     echo $DATA_URL > icon-$size.txt
 done
+magick logo-filled.png -resize 16x16 -resize 32x32 -resize 64x64 -resize 128x128 -colors 256 favicon.ico
 popd > /dev/null
