d4e82a6c0e5e12523bf83af0d31ec4580cfad202
æLtorio <aeltorio@universidadatlantico.org>
2024-10-22 16:35:12 +0200

    update favicon

--- gen_icon.sh
+++ gen_icon.sh
@@ -10,5 +10,10 @@ for size in 16 32 64 80 128; do
     DATA_URL="data:image/png;base64,$(base64 -i icon-$size.png)"
     echo $DATA_URL > icon-$size.txt
 done
-magick logo-filled.png -resize 16x16 -resize 32x32 -resize 64x64 -resize 128x128 -colors 256 favicon.ico
+magick logo-filled.png   -bordercolor white -border 0 \
+          \( -clone 0 -resize 16x16 \) \
+          \( -clone 0 -resize 32x32 \) \
+          \( -clone 0 -resize 48x48 \) \
+          \( -clone 0 -resize 64x64 \) \
+          -delete 0 -alpha off -colors 256 favicon.ico
 popd > /dev/null
